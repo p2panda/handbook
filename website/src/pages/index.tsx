be91fbe9ec9503403426339e6e018cba75b837d8
Andreas Dzialocha <x12@adz.garden>
2022-08-25 10:59:57 +0200

    Work on landing page, add pentagons with radial gradient

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,17 +1,89 @@
-import React from 'react';
 import Layout from '@theme/Layout';
+import React from 'react';
+import clsx from 'clsx';
 import { ZooAdventures } from 'zoo-adventures';
+
+import styles from './index.module.css';
+
+const CYAN = '#81e0ff';
+
+function Pentagon(props: {
+  size: number;
+  color: string;
+  position: 'left' | 'right';
+  margin: number;
+  hideOnSmallDevices?: boolean;
+}): JSX.Element {
+  const position =
+    props.position === 'left'
+      ? { left: -(props.size / props.margin) }
+      : { right: -(props.size / props.margin) };
+
+  return (
+    <div
+      className={clsx(styles['pentagon-container'], {
+        [styles['pentagon-container-small-hide']]: props.hideOnSmallDevices,
+      })}
+      style={{ height: props.size }}
+    >
+      <div className={styles['pentagon-position']} style={position}>
+        <svg width={props.size} height={props.size} viewBox="0 0 100 100">
+          <defs>
+            <radialGradient cy="55%" id="gradient">
+              <stop offset="40%" stopColor="white" />
+              <stop offset="80%" stopColor={props.color} />
+              <stop offset="100%" stopColor="white" />
+            </radialGradient>
+          </defs>
+          <polygon
+            points="26,86 11.2,40.4 50,12.2 88.8,40.4 74,86"
+            fill="url('#gradient')"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Zoo(): JSX.Element {
+  return (
+    <div className="panda-zoo">
+      <div className="panda-zoo-inner">
+        <ZooAdventures />
+      </div>
+    </div>
+  );
+}
+
+function Title(): JSX.Element {
+  return (
+    <>
+      <h1 className={styles['title']}>
+        p2panda is a protocol for local-first applications
+      </h1>
+      <p className={styles['subtitle']}>
+        Read more about it in the <a href="">introduction</a>
+      </p>
+    </>
+  );
+}
 
 export default function Home(): JSX.Element {
   return (
     <Layout
       title="Home"
-      description="Description will go into a meta tag in <head />">
-      <h1>p2panda is a protocol for local-first applications</h1>
-      <div className="panda-zoo">
-        <div className="panda-zoo-inner">
-          <ZooAdventures />
-        </div>
+      description="Description will go into a meta tag in <head />"
+    >
+      <div className={styles['wrapper']}>
+        <Pentagon size={1000} color={CYAN} position="right" margin={4} />
+        <Pentagon
+          size={500}
+          color={CYAN}
+          position="left"
+          margin={6}
+          hideOnSmallDevices
+        />
+        <Title />
       </div>
     </Layout>
   );
--- website/src/theme/Navbar/MobileSidebar/Toggle/index.js
+++ website/src/theme/Navbar/MobileSidebar/Toggle/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ThemedImage from '@theme/ThemedImage';
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import {useNavbarMobileSidebar} from '@docusaurus/theme-common/internal';
-import {useThemeConfig} from '@docusaurus/theme-common';
+import { useNavbarMobileSidebar } from '@docusaurus/theme-common/internal';
+import { useThemeConfig } from '@docusaurus/theme-common';
 
 // This shows our "Wild Plant Book" instead of the hamburger menu
 export default function MobileSidebarToggle() {
   const mobileSidebar = useNavbarMobileSidebar();
 
   const {
-    navbar: {title, logo},
+    navbar: { title, logo },
   } = useThemeConfig();
 
   const sources = {
@@ -24,9 +24,10 @@ export default function MobileSidebarToggle() {
       aria-label="Navigation bar toggle"
       className="navbar__toggle clean-btn"
       type="button"
-      tabIndex={0}>
-      <div class="navbar__brand">
-        <div class="navbar__logo">
+      tabIndex={0}
+    >
+      <div className="navbar__brand">
+        <div className="navbar__logo">
           <ThemedImage
             className={logo.className}
             sources={sources}
