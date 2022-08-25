e89b22f72d628b7e7da8ddb0e539361b1f3f9e87
Andreas Dzialocha <x12@adz.garden>
2022-08-25 09:29:38 +0200

    Embed the zoo

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import { ZooAdventures } from 'zoo-adventures';
 
 export default function Home(): JSX.Element {
   return (
     <Layout
       title="Home"
       description="Description will go into a meta tag in <head />">
+      <h1>p2panda is a protocol for local-first applications</h1>
+      <div className="panda-zoo">
+        <div className="panda-zoo-inner">
+          <ZooAdventures />
+        </div>
+      </div>
     </Layout>
   );
 }
