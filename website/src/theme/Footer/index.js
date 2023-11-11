ad98d6924ff6cd7177f67a31914710a158c6e1dc
adz <x12@adz.garden>
2023-11-11 12:14:29 +0100

    One more grant

--- website/src/theme/Footer/index.js
+++ website/src/theme/Footer/index.js
@@ -51,7 +51,8 @@ function Footer() {
         <p className={styles['footer-text']}>
           This project has received funding from the European Union’s Horizon
           2020 research and innovation programme within the framework of the
-          NGI-POINTER Project funded under grant agreement No 871528
+          NGI-POINTER Project funded under grant agreement No 871528,
+          NGI-ASSURE No 957073 and NGI0-ENTRUST No 101069594
         </p>
       </div>
       <p className={styles['footer-text']}>
