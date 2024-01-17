949b60b91b7bc43ae5d325537bdbee047d6432b2
adzialocha <adzialocha@users.noreply.github.com>
2024-01-17 16:33:46 +0000

    deploy: 469aed4006537fa33e3f706cb6b49e9072e26f37

---
+++ assets/js/5e95c892.1baf51bf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkhandbook=self.webpackChunkhandbook||[]).push([[9661],{4942:(e,s,r)=>{r.r(s),r.d(s,{default:()=>h});r(7294);var a=r(512),n=r(5463),u=r(3702),c=r(8790),o=r(5337),d=r(5893);function h(e){return(0,d.jsx)(n.FG,{className:(0,a.Z)(u.k.wrapper.docsPages),children:(0,d.jsx)(o.Z,{children:(0,c.H)(e.route.routes)})})}}}]);
